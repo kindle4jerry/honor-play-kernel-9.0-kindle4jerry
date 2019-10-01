ce234e43fc89a003d5129df4d478623846b9098d
kindle4jerry <55440446+kindle4jerry@users.noreply.github.com>
2019-10-01 15:29:12 +0800

    Update fight.sh

--- kernel/fight.sh
+++ kernel/fight.sh
@@ -2,14 +2,17 @@
 
 #注释很详细的
 
-#开头环境配置，可以参考官方文档，也可以把我的路径改改
+#开头环境配置，可以参考官方文档，我在此简单汉化搬运，也可以把我的路径改改
+#你需要想想办法比如谷歌搜索去搞一个aarch64-linux-android-4.9这个玩意，然后把下面的路径改一改
 
 export PATH=$PATH:/home/lumia/aarch64-linux-android-4.9/bin
 export CROSS_COMPILE=aarch64-linux-android-
 
 ##以下是官方文档给出的命令 看不懂可以不改
+# 到上一层开一个out文件夹
 mkdir ../out
 
+#照搬
 make ARCH=arm64 O=../out merge_kirin970_defconfig
 
 
@@ -17,3 +20,4 @@ make ARCH=arm64 O=../out merge_kirin970_defconfig
 
 make ARCH=arm64 O=../out -j40
 
+#最终输出 Kernel : out/arch/arm64/boot/Image.gz
