8e1eede3e57cd0c7428b3e771576de60302a12a1
kindle4jerry <55440446+kindle4jerry@users.noreply.github.com>
2019-11-03 00:36:20 +0800

    Update fight.sh

--- kernel/fight.sh
+++ kernel/fight.sh
@@ -4,7 +4,8 @@
 
 #开头环境配置，可以参考官方文档，我在此简单汉化搬运，也可以把我的路径改改
 #你需要想想办法比如谷歌搜索去搞一个aarch64-linux-android-4.9这个玩意，然后把下面的路径改一改
-#懒人可以考虑从我的服务器搞一个aarch啥的，不知道能不能用 http://104.143.94.60/usr/uploads/pans/aarch64.tar.gz 小网站，貌似不支持断点续传，貌似没啥防护，做人要良心啊，我这无广告免费满速下载啊，别有事没事来捶我服务器。
+#懒人可以考虑从我的服务器搞一个aarch啥的，不知道能不能用
+#http://104.143.94.60/usr/uploads/pans/aarch64.tar.gz 小网站，貌似没啥防护，做人要厚道啊，我这无广告免费满速下载啊，别有事没事来捶我服务器就行。
 
 export PATH=$PATH:/home/lumia/aarch64-linux-android-4.9/bin
 export CROSS_COMPILE=aarch64-linux-android-
