5d0a3dea5ca9d2afb8be1e0000c62b7567bcdd74
kindle4jerry <55440446+kindle4jerry@users.noreply.github.com>
2019-11-09 00:18:13 +0800

    Update fight.sh

--- kernel/fight.sh
+++ kernel/fight.sh
@@ -6,6 +6,7 @@
 #你需要想想办法比如谷歌搜索去搞一个aarch64-linux-android-4.9这个玩意，然后把下面的路径改一改
 #懒人可以考虑从我的服务器搞一个aarch啥的，不知道能不能用
 #http://104.143.94.60/usr/uploads/pans/aarch64.tar.gz 小网站，貌似没啥防护，做人要厚道啊，我这无广告免费满速下载啊，别有事没事来捶我服务器就行。
+#下面第一行根据你的aarch的位置设置，第二行照抄
 
 export PATH=$PATH:/home/lumia/aarch64-linux-android-4.9/bin
 export CROSS_COMPILE=aarch64-linux-android-
@@ -25,3 +26,4 @@ make ARCH=arm64 O=../out -j40
 
 #最终输出 Kernel 的位置: out/arch/arm64/boot/Image.gz
 #然后把Image.gz送去打包，位置/kernel/tool
+#打包那里有pk.sh也可以看注释
